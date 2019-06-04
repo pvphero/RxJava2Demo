a2fc9407e4dec25284d33befe88b06ed249f4fd9
pvphero <impvphero@163.com>
2019-06-04 17:53:19 +0800

    add ignore

--- app/src/main/java/com/pro/rxjavademo/errorhandling/OnErrorActivity.java
+++ app/src/main/java/com/pro/rxjavademo/errorhandling/OnErrorActivity.java
@@ -1,6 +1,7 @@
 package com.pro.rxjavademo.errorhandling;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.pro.rxjavademo.BaseActivity;
 import com.pro.rxjavademo.R;
@@ -35,6 +36,14 @@ public class OnErrorActivity extends BaseActivity {
                         log("onErrorReturn-onCompleted");
                     }
                 }));
+
+        mRButton.setText(getString(R.string.on_error_resume));
+        mRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private Observable<String> onErrorReturnObserver() {
